cf0f761a397b6abe869a848afe7838ee7aa9911e
Robert Drey <dreyrobert@gmail.com>
2023-03-17 20:37:50 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/fronteiratec/',
+    base: '/site-fronteira-tec-2/',
     plugins: [react()]
 })
