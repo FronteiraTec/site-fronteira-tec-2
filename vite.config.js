7d4774760f3ec90e4ebf63fbdd69c701ba4bb62a
Robert Drey <dreyrobert@gmail.com>
2023-03-17 20:42:49 -0300

    build

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({})
+export default defineConfig({
+    base: '/site-fronteira-tec-2/',
+    plugins: [react()]
+})
+
